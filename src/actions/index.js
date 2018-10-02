ae6273770293e185e097ab7d623134f9222075ae
Eliot Charette <eliotcharette@gmail.com>
2018-10-01 20:47:01 -0700

    fix bug in creatpost action

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,9 +16,9 @@ export function fetchPosts() {
 
 
 export function createPost(values) {
-  const request = axios.post(`{ROOT_URL}/posts${API_KEY}`,values);
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`,values);
   return {
     type: CREATE_POST,
     payload: request
-  }
+  };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -79,5 +79,5 @@ export default reduxForm({
   validate,
   form: 'PostsNewForm'
 })(
-  connect(null, { createPost })(PostsNew)
+  connect(null,{ createPost })(PostsNew)
 );
