922e32ac75fa2dd84179219ac0ec0ffd0028eb93
Guest User <Guest@epicodus-19.local>
2018-10-04 09:07:58 -0700

    Continue with lesson 99

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,6 +3,17 @@ import { FETCH_POSTS } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+    //es5 syntax
+    //const post = action.payload.data;
+    // const newState = { ...state }
+    // newState[post.id] = post;
+    // return newState;
+
+    //es6 syntax
+    return { ...state, [action.payload.data.id]: action.payload.data };
+
+
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
