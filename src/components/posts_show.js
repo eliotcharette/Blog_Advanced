47d6c3b692dae5026461be925d7502138d89fd43
Guest User <Guest@epicodus-19.local>
2018-10-04 14:08:45 -0700

    99 min 5

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
+
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost();
+  }
+
+
   render() {
     return (
       <div>
@@ -10,4 +19,8 @@ class PostsShow extends Component {
   };
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }) {
+
+}
+
+export default connect(null, {fetchPost}) (PostsShow);
